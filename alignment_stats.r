e70cdbd718a7d7f2889096260583fcee32bde079
roblanf <rob.lanfear@gmail.com>
2016-09-08 15:39:10 +1000

    clean unused files

--- alignment_stats.r
+++
@@ -1,8 +0,0 @@
-library('seqinr')
-library('ape')
-
-input = '~/Documents/github/PartitionedAlignments/datasets/Duchene_2015a/alignment.nex'
-
-al <- as.alignment(read.nexus.data(input))
-
-GC.content(as.DNAbin(al))
