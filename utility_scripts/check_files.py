be981791fa349c1bda7fd3894a345cb919e1cc02
roblanf <rob.lanfear@gmail.com>
2018-09-16 13:31:54 +1000

    remove splitting from check_files.py

--- utility_scripts/check_files.py
+++ utility_scripts/check_files.py
@@ -27,7 +27,6 @@ for folder in dataset_folders:
 
     folder = os.path.realpath(folder) # prettier
     logging.info("Checking %s" %folder)
-    splitflag = 0
     
     # 1. Check that the two files exist, at least in principle (maybe they're empty)
     files = os.listdir(folder)
@@ -36,14 +35,6 @@ for folder in dataset_folders:
         raise ValueError
     logging.info("    found YAML file")
 
-    if files.count("alignment_split") == 1: #only here if the alignment is >100MB
-    	# so now we make the alignment.nex file so we can check it
-        logging.info("    joining split alignments into single alignment.nex")
-        command = "cat %s > %s" %(os.path.join(folder, 'alignment_split', 'alignment.nex.*'), os.path.join(folder, 'alignment.nex'))
-        os.system(command)
-        splitflag = 1
-        files = os.listdir(folder)
-
     if files.count("alignment.nex") != 1:
         logging.error("couldn't find alignment.nex file for %s" %folder)
         raise ValueError
@@ -51,7 +42,7 @@ for folder in dataset_folders:
     logging.info("    found alignment.nex file")
 
     # clean up the folder: remove all files except the two we want
-    extras = set(files) - set(['alignment_split', 'alignment.nex', 'README.yaml', 'alignment.nex-seq-summary.txt', 'alignment.nex-summary.txt'])
+    extras = set(files) - set(['alignment.nex', 'README.yaml', 'alignment.nex-seq-summary.txt', 'alignment.nex-summary.txt'])
     if extras:
         logging.info("    removing %d additional file(s)", len(extras))
         for f in extras:
@@ -68,22 +59,6 @@ for folder in dataset_folders:
 
     alignment_file = os.path.join(folder, "alignment.nex")
     aln = check_alignment(alignment_file)
-
-    # 5. split up large alignment files if necessary
-    # this is coded so that we split up any alignment >100MB, since that's the github limit
-    if os.path.getsize(os.path.join(folder, "alignment.nex")) >= 100000000.0:
-
-        if splitflag == 0:
-            logging.info("    splitting alignment because it's larger than 100MB")
-            os.system('mkdir %s' %(os.path.join(folder, 'alignment_split')))
-            command = 'split -b 50m %s %s' %(os.path.join(folder, "alignment.nex"), os.path.join(folder, 'alignment_split', 'alignment.nex.'))
-            os.system(command)
-
-        # remove the original, but only if we managed to zip it successfully
-        files = os.listdir(os.path.join(folder, 'alignment_split'))
-        if files.count("alignment.nex.aa") == 1:
-            logging.info("    removing large raw alignment file")
-            os.system("rm %s" %(os.path.join(folder, "alignment.nex")))
 
     logging.info("    done, no errors found")
 
