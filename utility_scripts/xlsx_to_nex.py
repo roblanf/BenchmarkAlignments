f17a4f1f50fde0a0ef435cb7f1fb59765902e7cc
HuaiyanRen <928003585@qq.com>
2023-09-07 11:23:55 +1000

    Update xlsx_to_nex.py
    Fix some format problems, make it really works with IQ-TREE.

--- utility_scripts/xlsx_to_nex.py
+++ utility_scripts/xlsx_to_nex.py
@@ -2,7 +2,12 @@ import os
 import pandas as pd
 import argparse
 
-def xlsx_to_nex(inpath):
+def xlsx_to_nex(inpath,seq_type):
+    if seq_type == 'DNA':
+        model = 'GTR+I+G'
+    elif seq_type == 'AA':
+        model = 'LG+I+G'
+        
     
     xlsx_file = os.path.join(inpath, 'charset.xlsx')
     
@@ -10,11 +15,11 @@ def xlsx_to_nex(inpath):
         df = pd.read_excel(xlsx_file, sheet_name= set_sheet , engine='openpyxl')
         
         
-        nex_list = ['#NEXUS', 'begin set;','','['+set_sheet+']']
+        nex_list = ['#NEXUS', 'begin sets;','','['+set_sheet+']']
         charpar_str = 'charpartition ' + set_sheet + ' ='
         for i in range(len(df)):
             nex_list.append('charset ' + df.charset[i] + ' = ' + str(df.start[i]) + '-' + str(df.end[i]) + ';')
-            charpar_str = charpar_str + ' ' + df.charset[i] + ': ' + str(df.start[i]) + '-' + str(df.end[i]) + ','
+            charpar_str = charpar_str + ' ' + model + ': ' + df.charset[i] + ','
         charpar_str = charpar_str[:-1] + ';'
         nex_list = nex_list + ['', charpar_str, '', 'end;']
 
@@ -30,10 +35,12 @@ def xlsx_to_nex(inpath):
 parser = argparse.ArgumentParser(description='')
 parser.add_argument('--inpath', '-i', help='', 
                     default = r"C:\Users\u7151703\Desktop\research\datasets\processing\nex\datasets")
+parser.add_argument('--seq_type', '-st', help='', 
+                    required= True)
 args = parser.parse_args()
 
 if __name__ == '__main__':
     try:
-       xlsx_to_nex(args.inpath)
+       xlsx_to_nex(args.inpath, args.seq_type)
     except Exception as e:
         print(e)    
