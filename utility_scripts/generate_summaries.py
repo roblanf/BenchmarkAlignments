87680ccc2d6e88ebf81b493d8bb0b6aef4a3f555
HuaiyanRen <928003585@qq.com>
2023-09-11 16:22:53 +1000

    Update generate_summaries.py
    implement argparse module

--- utility_scripts/generate_summaries.py
+++ utility_scripts/generate_summaries.py
@@ -1,45 +1,41 @@
 # generates AMAS summaries for all datasets, only where they are missing
-
-#import AMAS
 from Bio.Nexus import Nexus
 import os
 import shutil
-#import glob
-
-cores = 4
-
-#all_files = glob.glob(os.path.abspath(os.path.join(os.getcwd(),'..','datasets','**')), recursive=False)
-
-#print(all_files)
+import argparse
 
 
-
-f = r"C:\Users\u7151703\Desktop\research\datasets\processing\nex\datasets"
-    
-alnf = os.path.join(f, 'alignment.nex')
+def gen_smy(inpath, cores):        
+    alnf = os.path.join(inpath, 'alignment.nex')        
+    sumf = os.path.join(inpath, "alignment.nex-summary.txt")
+    taxf = os.path.join(inpath, "alignment.nex-seq-summary.txt")
         
-print(alnf)
-    
-sumf = os.path.join(f, "alignment.nex-summary.txt")
-taxf = os.path.join(f, "alignment.nex-seq-summary.txt")
-    
-if os.path.isfile(sumf)==True and os.path.isfile(taxf)==True:
-    pass
-else:
-    # get the data type
-    dat = Nexus.Nexus()
-    dat.read(alnf)
-    if dat.datatype == 'nucleotide':
-        data_type = 'dna'
-    if dat.datatype == 'protein':
-        data_type = 'aa'
-        
-    # get the AMAS summaries
-    #aln = AMAS.MetaAlignment(in_files=[alnf], data_type=data_type,in_format="nexus", cores=cores) #bug
-    #aln.write_summaries(sumf)
-    #aln.write_taxa_summaries(taxf)
+    if os.path.isfile(sumf)==True and os.path.isfile(taxf)==True:
+        pass
+    else:
+        # get the data type
+        dat = Nexus.Nexus()
+        dat.read(alnf)
+        if dat.datatype == 'nucleotide':
+            data_type = 'dna'
+        if dat.datatype == 'protein':
+            data_type = 'aa'
+               
+        cmd = r'python C:\Users\u7151703\Desktop\research\code\BenchmarkAlignments\utility_scripts\AMAS.py summary -f nexus -d '+data_type+' -i '+alnf+' -c '+str(cores)+' -s'   
+        os.system(cmd)
+        shutil.move('summary.txt', sumf)
 
-    cmd = r'python C:\Users\u7151703\Desktop\research\code\BenchmarkAlignments\utility_scripts\AMAS.py summary -f nexus -d '+data_type+' -i '+alnf+' -c '+str(cores)+' -s'   
-    os.system(cmd)
-    shutil.move('summary.txt', sumf)
 
+# running
+parser = argparse.ArgumentParser(description='')
+parser.add_argument('--inpath', '-i', help='', 
+                    default = r"C:\Users\u7151703\Desktop\research\datasets\processing\nex\datasets")
+parser.add_argument('--cores', '-c', help='', 
+                    default = 1)
+args = parser.parse_args()
+
+if __name__ == '__main__':
+    try:
+       gen_smy(args.inpath, args.cores)
+    except Exception as e:
+        print(e)  
