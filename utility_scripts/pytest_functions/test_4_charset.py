c4fba834d4552e88d541a4d86c81acb346ba7c3b
HuaiyanRen <928003585@qq.com>
2023-12-05 10:46:09 +1100

    Update test_4_charset.py

--- utility_scripts/pytest_functions/test_4_charset.py
+++ utility_scripts/pytest_functions/test_4_charset.py
@@ -1,10 +1,11 @@
 # python -m pytest, test cmd
 # python -m pytest -x, stop after one failure
 # python -m pytest -s, see print messages
-# python -m pytest > ..path\pytest_log.txt, record the output. Mine: C:\Users\u7151703\Desktop\research\datasets\processing\nex\datasets\pytest_log.txt
+# python -m pytest > ..path\pytest_log.txt, record the output.
 import os
 import pandas as pd
 from Bio.Nexus import Nexus
+import numpy as np
 import itertools
 from datetime import datetime
  
@@ -17,64 +18,50 @@ df = pd.read_csv(charset_file)
 
 aln = Nexus.Nexus()
 aln.read(alignment_file)
-all_sites = set(range(aln.nchar))
+all_sites = set(range(1, aln.nchar + 1))
 
 def test_charpart_keys():
     x = df   
     
-    assert 'partitions' in x.columns, "There should be a column named 'partitions'."
-    assert 'loci' in x.columns, "There should be a column named 'loci'."
-    assert 'genomes' in x.columns, "There should be a column named 'genomes'."
+    assert 'alignment_name' in x.columns, "There should be a column named 'alignment_name'."
+    assert 'partition_name' in x.columns, "There should be a column named 'partition_name'."
+    assert 'partition_start' in x.columns, "There should be a column named 'partition_start'."
+    assert 'partition_end' in x.columns, "There should be a column named 'partition_end'."
+    assert 'partition_skip' in x.columns, "There should be a column named 'partition_skip'."
+    assert 'locus_name' in x.columns, "There should be a column named 'locus_name'."
+    assert 'codon_position' in x.columns, "There should be a column named 'codon_position'."
+    assert 'genome' in x.columns, "There should be a column named 'genome'."
+    assert 'data_type' in x.columns, "There should be a column named 'data_type'."
     print('\n\tcharset columns correct')
     
 
 def test_partititons():
-    x = df.dropna(subset=['partitions'])
+    x = df
     
-    site_length = 0
     sites_range = []
     for i in range(len(x)):
-        site_length = site_length + x.par_end[i] + 1 - x.par_start[i]
-        sites_range.append(range(int(x.par_start[i]) - 1, int(x.par_end[i])))
-    sites = set((itertools.chain.from_iterable(sites_range)))
+        sites_list = list(np.arange(x.partition_start[i],x.partition_end[i] + 1, x.partition_skip[i]))
+        sites_range = sites_range + sites_list
+    sites = set(sites_range)
     
-    #assert site_length == len(all_sites), "The partitions charpartition has {} more/less site(s) than the number of sites in the alignment".format(abs(site_length - len(all_sites)))
     assert all_sites.difference(sites) == set(), "The partitions charpartition does not cover the following sites: {}".format(all_sites.difference(sites))     
     assert sites.difference(all_sites) == set(), "The partitions charpartition has unexpected sites: {}".format(sites.difference(all_sites)) 
     print('\tpartitions sites correct')
 
 
 def test_loci():
-    x = df.dropna(subset=['loci'])
+    x = df
     
-    site_length = 0
-    sites_range = []
     for i in range(len(x)):
-        site_length = site_length + x.loci_end[i] + 1 - x.loci_start[i]
-        sites_range.append(range(int(x.loci_start[i]) - 1, int(x.loci_end[i])))
-    sites = set((itertools.chain.from_iterable(sites_range)))
-    
-    assert site_length == len(all_sites), "The loci charpartition has {} more/less site(s) than the number of sites in the alignment".format(abs(site_length - len(all_sites)))
-    assert all_sites.difference(sites) == set(), "The loci charpartition does not cover the following sites: {}".format(all_sites.difference(sites))     
-    assert sites.difference(all_sites) == set(), "The loci charpartition has unexpected sites: {}".format(sites.difference(all_sites)) 
+        assert x.partition_name[i].split('_')[0] == x.locus_name[i], "The partition name {} doesn't match the locus name.".format(x.partition_name[i].split('_')[0])
     print('\tloci sites correct')
 
 
 def test_genomes():
-    x = df.dropna(subset=['genomes'])
+    x = df
     candi_gen_list = ['bacterial', 'chloroplast', 'dsDNA', 'dsRNA', 'mitochondrial', 'nuclear', 'ssDNA', 'ssRNA']
-    
-    site_length = 0
-    sites_range = []
-    for i in range(len(x)):
-        site_length = site_length + x.gen_end[i] + 1 - x.gen_start[i]
-        sites_range.append(range(int(x.gen_start[i]) - 1, int(x.gen_end[i])))
-    sites = set((itertools.chain.from_iterable(sites_range)))
-    
-    assert all(gen in candi_gen_list for gen in x.genomes), "unexpect name(s) in charset genomes: {}".format([gen for gen in x.genomes if gen not in candi_gen_list])
-    assert site_length == len(all_sites), "The genomes charpartition has {} more/less site(s) than the number of sites in the alignment".format(abs(site_length - len(all_sites)))
-    assert all_sites.difference(sites) == set(), "The genomes charpartition does not cover the following sites: {}".format(all_sites.difference(sites))     
-    assert sites.difference(all_sites) == set(), "The genomes charpartition has unexpected sites: {}".format(sites.difference(all_sites)) 
+     
+    assert all(gen in candi_gen_list for gen in x.genome), "unexpect name(s) in charset genomes: {}".format([gen for gen in x.genomes if gen not in candi_gen_list])
     print('\tgenomes sites correct')
 
 now = datetime.now()
